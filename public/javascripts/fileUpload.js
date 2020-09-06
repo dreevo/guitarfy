7f6e0da56ef074e39936a85bb500eeb817485a32
dreevo <sidedark85@gmail.com>
2020-09-06 16:51:12 +0100

    Finished project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,12 +1,31 @@
-FilePond.registerPlugin(
-  FilePondPluginImagePreview,
-  FilePondPluginImageResize,
-  FilePondPluginFileEncode
-);
-FilePond.parse(document.body);
+const routeStyle = window.getComputedStyle(document.documentElement);
+if (
+  routeStyle.getPropertyValue("--guitar-model-width-large") != null &&
+  routeStyle.getPropertyValue("--guitar-model-width-large") != ""
+) {
+  ready();
+} else {
+  document.getElementById("main-css").addEventListener("load", ready);
+}
 
-FilePond.setOptions({
-  stylePanelAspectRatio: 150 / 100,
-  imageResizeTargetWidth: 100,
-  imageResizeTargetHeight: 150,
-});
+function ready() {
+  const coverWidth = parseFloat(
+    routeStyle.getPropertyValue("--guitar-model-width-large")
+  );
+  const coverAspectRatio = parseFloat(
+    routeStyle.getPropertyValue("--guitar-model-aspect-ratio")
+  );
+  const coverHeight = coverWidth / coverAspectRatio;
+  FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode
+  );
+
+  FilePond.setOptions({
+    stylePanelAspectRatio: 1 / coverAspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight,
+  });
+  FilePond.parse(document.body);
+}
